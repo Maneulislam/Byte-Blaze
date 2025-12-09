defc6d292e4705c4cd1e8c72d647bb7bd1515738
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-12-09 18:21:13 +0600

    Project Completed

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import React, { useEffect, useState } from 'react';
 import BlogCard from '../component/BlogCard';
 import { deleteBlog, getBlog } from '../utilities';
